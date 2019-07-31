10793b19e5fa73376457278cd421ffd691be4cb2
guruvyasa <guruvyasa@yahoo.co.in>
2019-07-31 13:26:09 +0530

    Added spaceship and movements

--- game.js
+++ game.js
@@ -1,13 +1,47 @@
 // import Game from "Game";
 window.onload = function(){
-    console.log("hello");
-    class Game{
+    class SpaceShip{
         constructor(){
+            this.width = 100;
+            this.height = 100;
+            this.speed = 2;
+            this.position = {x:100, y:450};
+            this.img = new Image(this.width,this.height);
+            this.img.src = "assets/spaceship.png";
+        }
+        move(keycode){
+            console.log(keycode);
+            switch(keycode){
+                case 37:this.position.x -= this.speed;
+                        break;
+                case 38:this.position.y -= this.speed;
+                        break;
+                case 39:this.position.x += this.speed;
+                        break;
+                case 40:this.position.y += this.speed; 
+            }
+        }
+
+        draw(ctx){
+            ctx.save();
+            ctx.translate(this.position.x, this.position.y);
+            ctx.drawImage(this.img, 
+                        -this.width/2, 
+                        -this.height/2, 
+                         this.width,this.height);
+            ctx.restore();
+        }
+    }
+   
+    class Game{
+        constructor(ctx){
             this.stopped = true;
+            this.ctx = ctx;
+            this.spaceShip = new SpaceShip();
+            this.initEventHandlers();
         }
     
         drawStartScreen(ctx){
-            console.log(this.stopped);
             if(this.stopped){
                 ctx.save();
                 ctx.translate(300,300);
@@ -18,11 +52,31 @@ window.onload = function(){
                 ctx.restore();
             }
         }
+        update(){
+            requestAnimationFrame(()=>this.update());
+            this.ctx.clearRect(0,0,600,600);
+            this.spaceShip.draw(this.ctx);
+
+        }
+
+        initEventHandlers(){
+            //handle spacebar
+            document.addEventListener('keydown',(e)=>{
+                // console.log(e.keyCode);
+                if (e.keyCode == 32 && this.stopped){
+                    this.stopped = false;
+                    requestAnimationFrame(()=>this.update());
+                }
+                else if(e.keyCode >= 37 && e.keyCode <= 40 && !this.stopped){
+                    this.spaceShip.move(e.keyCode);
+                }
+            })
+        }
         
     }
     let ctx = document.querySelector("#gameScreen").getContext("2d");
     // console.log(ctx);
     // ctx.fillRect(200,200,200,100);
-    let game = new Game();
+    let game = new Game(ctx);
     game.drawStartScreen(ctx);
 }
